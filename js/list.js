a3b17ccd66455c44decdcc8a36f19339833be990
Xuan Huy <xuanhuy900@gmail.com>
2020-08-29 19:54:02 +0700

    Delete and update data

--- js/list.js
+++ js/list.js
@@ -24,7 +24,6 @@ $(document).ready(function(){
         }
         return response.json();
     }).then(function(data){
-        console.log(data);
         data.forEach(element => {
             content += `<tr id="${element.Id}" role="button" data-toggle="modal" data-target="#detailModal">
                 <td id='FullName'>${element.FullName}</td>
@@ -72,14 +71,124 @@ $(document).ready(function(){
            });
        }
        //get element
-        $('#table tbody tr').on('click', function () {
-            console.log($(this).children('#Address').text());
-            var content = `
-                
-
-
-            `;
+       $('#table tbody tr').on('click', function () {
+        var id = $(this).attr('id');
+        localStorage.setItem('id',id);
+        var getUrl = 'https://learn.landsoftapis.com/api/Customer/Get?Id='+id;
+        fetch(getUrl, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': 'Bearer ' + idToken,
+                },    
+        }).then(function(response){
+            if(response.status != 200){
+                throw Error(status);
+            }
+            return response.json();
+        }).then(function(data){
+                $('#firstName').val(data.FirstName);
+                $('#lastName').val(data.LastName);
+                $('#phone').val(data.Mobile);
+                $('#email').val(data.Email);
+                $('#position').val(data.Position);
+                $('#iDNo').val(data.IDNo);
+                $('#dateOfIssue').val(data.DateOfIssue);
+                $('#placeOfIssue').val(data.PlaceOfIssue);
+                $('#dataOfBirth').val(data.DateOfBirth);
+                $('#address1').val(data.Address1);
+                $('#address2').val(data.Address2);
+                $('#companyName').val(data.CompanyName);
+                $('#companyTaxCode').val(data.CompanyTaxCode);
+                $('#companyAddress').val(data.CompanyAddress);
+        }).catch(function(err){
+            console.log(err);
         });
+        var UpdateUrl = 'https://learn.landsoftapis.com/api/Customer/Update?Id='+id;
+        console.log(UpdateUrl);
+        $('#update').on('click', function(){
+            var firstName = $('#firstName').val();
+            var lastName = $('#lastName').val();
+            var phone = $('#phone').val();
+            var email = $('#email').val();
+            var position = $('#position').val();
+            var iDNo = $('#iDNo').val();
+            var dateOfIssue = $('#dateOfIssue').val();
+            var placeOfIssue = $('#placeOfIssue').val();
+            var dataOfBirth = $('#dataOfBirth').val();
+            var address1 = $('#address1').val();
+            var address2 = $('#address2').val();
+            var companyName = $('#companyName').val();
+            var companyTaxCode = $('#companyTaxCode').val();
+            var companyAddress = $('#companyAddress').val();
+            var data = {
+                "LastName": lastName,
+                "FirstName": firstName,
+                "ShortName": "string",
+                "Mobile": phone,
+                "GroupId": 0,
+                "Address1": address1,
+                "Address2": address2,
+                "yyyy2": dateOfIssue,
+                "PlaceOfIssue": placeOfIssue,
+                "JobId": 0,
+                "Company": companyName,
+                "Position": position,
+                "Email": email,
+                "Note": '',
+                "StaffId": 0,
+                "dd": "string",
+                "MM": "string",
+                "yyyy": dataOfBirth,
+                "IsPersonal": true,
+                "CompanyName": companyName,
+                "CompanyTaxCode": companyTaxCode,
+                "CompanyAddress": companyAddress,
+                "UID": iDNo,
+            }
+            fetch(UpdateUrl, {
+                method: 'PUT',
+                headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': 'Bearer ' + idToken,
+                },
+                body: JSON.stringify(data)
+            }).then(function(response){
+                if(response.status != 200){
+                    throw Error(status);
+                }
+                return response.json();
+            }).then(function(data){
+                alert('Cập nhật tài khoản thành công!');
+                console.log('Success!');
+            }).catch(function(err){
+                //alert('Cập nhật thất bại xin thử lại!');
+                console.log(err);
+            });
+        });
+        var delUrl = 'https://learn.landsoftapis.com/api/Customer/Delete?Id='+id;
+        $('#del').on('click', function(){
+            fetch(delUrl, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': 'Bearer ' + idToken,
+                },
+            }).then(function(response){
+                if(response.status != 200){
+                    throw Error(status);
+                }
+            }).then(function(data){
+                alert('Xóa tài khoản thành công!');
+                console.log('Success!')
+            }).catch(function(err){
+                alert('Không thể xóa tài khoản này!');
+            });
+        });
+    });
        // xảy ra lỗi
     }).catch(function(err){
         alert('Hết thời gian sử dụng');
@@ -89,23 +198,23 @@ $(document).ready(function(){
     // thêm mới người dùng 
     var addUrl = 'https://learn.landsoftapis.com/api/Customer/Insert';
     $('#submit').on('click', function(){
-        var firstName = $('#firstName').val();
-        var lastName = $('#lastName').val();
-        var phone = $('#phone').val();
-        var email = $('#email').val();
-        var position = $('#position').val();
-        var iDNo = $('#iDNo').val();
-        var dateOfIssue = $('#dateOfIssue').val();
-        var placeOfIssue = $('#placeOfIssue').val();
-        var dataOfBirth = $('#dataOfBirth').val();
-        var address1 = $('#address1').val();
-        var address2 = $('#address2').val();
-        var companyName = $('#companyName').val();
-        var companyTaxCode = $('#companyTaxCode').val();
-        var companyAddress = $('#companyAddress').val();
+        var firstName = $('#firstNameNew').val();
+        var lastName = $('#lastNameNew').val();
+        var phone = $('#phoneNew').val();
+        var email = $('#emailNew').val();
+        var position = $('#positionNew').val();
+        var iDNo = $('#iDNoNew').val();
+        var dateOfIssue = $('#dateOfIssueNew').val();
+        var placeOfIssue = $('#placeOfIssueNew').val();
+        var dataOfBirth = $('#dataOfBirthNew').val();
+        var address1 = $('#address1New').val();
+        var address2 = $('#address2New').val();
+        var companyName = $('#companyNameNew').val();
+        var companyTaxCode = $('#companyTaxCodeNew').val();
+        var companyAddress = $('#companyAddressNew').val();
         var data = {
-            "LastName": firstName,
-            "FirstName": lastName,
+            "LastName": lastName,
+            "FirstName": firstName,
             "ShortName": "string",
             "Mobile": phone,
             "GroupId": 0,
@@ -146,5 +255,8 @@ $(document).ready(function(){
             console.log(err);
         });
     }); 
-
+    $('#exit1, #exit2').on('click', function(){
+        location.reload();
+    });
+    
 });
