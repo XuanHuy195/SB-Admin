c3b18b7202000504fc4a4496e735bd296dfa074b
Xuan Huy <xuanhuy900@gmail.com>
2020-09-09 16:07:27 +0700

    Home Page

--- js/project.js
+++ js/project.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
         data.forEach(element => {
             content += `
             <div class="col-lg-3 col-sm-6 col-md-6 col-6 my-2">
-                <div class="card shadow shadow-card my-2 w-100 h-100">
+                <div class="card shadow-card my-2 w-100 h-100">
                     <img src="${element.ImageUrl}" class="card-img-top" style="width:100%"></img>
                     <div class="card-body">
                         <h4 class="card-title text-primary">${element.Name}</h4>
