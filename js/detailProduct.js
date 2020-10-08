421c80dcfe812947cbff87c5d89ac7261c8c9421
Xuan Huy <xuanhuy900@gmail.com>
2020-10-08 21:25:52 +0700

    Blocks

--- js/detailProduct.js
+++ js/detailProduct.js
@@ -1,4 +1,47 @@
 
 $(document).ready(function(){
-
+    
+    $('.loop').owlCarousel({
+        center: true,
+        items:2,
+        loop:true,
+        margin:10,
+        responsive:{
+            600:{
+                items:5
+            }
+        }
+    });
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        },
+        responsiveClass:true,
+        responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+    });
 })
